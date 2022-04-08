51706ca1757ad4e0f85b08289935601e96f97e4b
taimooraslam1 <taimoor.aslam@invozone.com>
2022-04-08 19:58:45 +0500

    CRUD with postgres sql

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+import Users from "../controllers/user";
+import Books from "../controllers/book";
+
+export default (app) => {
+  app.get("/api", (req, res) =>
+    res.status(200).send({
+      message: "Welcome to the bookStore API!",
+    })
+  );
+
+  app.post("/api/users", Users.signUp); // API route for user to signup
+  app.post("/api/users/:userId/books", Books.create); // API route for user to create a book
+};
